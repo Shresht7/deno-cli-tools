e324760723d4873d7ae337c20d91b51dac07d8e0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-01-31 21:05:10 +0530

    Create write helper function

--- helpers/index.ts
+++ helpers/index.ts
@@ -7,3 +7,14 @@ export const pipe = (...fns: ((s: string) => string)[]) => (s: string) => fns.re
 
 /** Composes the given functions */
 export const compose = (...fns: ((s: string) => string)[]) => (s: string) => fns.reduceRight((acc, currFn) => currFn(acc), s)
+
+//  WRITER
+//  ======
+
+const writer = (encoder = new TextEncoder()) => {
+    return (text: string) => Deno.stdout.write(encoder.encode(text))
+}
+
+//  -------------------------
+export const write = writer()
+//  -------------------------
