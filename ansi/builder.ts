99fe70a42c4a5f4395a6f7201f4b4cb7d881211a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-05-16 19:25:25 +0530

    Refactor(ANSI): Add type definition and comments to ansi builder

--- ansi/builder.ts
+++ ansi/builder.ts
@@ -2,13 +2,16 @@
 //  ANSI BUILDER
 //  ============
 
+/** Callback function that takes in a string and returns a modified version of that string */
+type StringModifier = (s: string) => string
+
 /** ANSI template string builder */
-export const ansi = (templateStr: TemplateStringsArray, ...rest: (string | ((str: string) => string))[]) => {
+export const ansi = (templateStr: TemplateStringsArray, ...rest: (string | StringModifier)[]) => {
     return templateStr.reduce((acc, curr, i) => {
 
         //  If the preceding parameter is a function, execute it on the current template string
         if (typeof rest[i - 1] === 'function') {
-            const res = (rest[i - 1] as (s: string) => string)(curr)
+            const res = (rest[i - 1] as StringModifier)(curr)
             acc += res
         }
 
