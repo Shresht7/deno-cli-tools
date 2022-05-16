45416e5e626f4493e72549cd6ae4b92cfaa5aac0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-05-16 14:57:56 +0530

    add generics to pipe and compose

--- helpers/composition.ts
+++ helpers/composition.ts
@@ -2,8 +2,10 @@
 //  FUNCTION COMPOSITION
 //  ====================
 
+type CallbackFunction<T> = (x: T) => T
+
 /** Pipes the given functions */
-export const pipe = (...fns: ((s: string) => string)[]) => (s: string) => fns.reduce((acc, currFn) => currFn(acc), s)
+export const pipe = <T>(...fns: CallbackFunction<T>[]) => (x: T) => fns.reduce((acc, currFn) => currFn(acc), x)
 
 /** Composes the given functions */
-export const compose = (...fns: ((s: string) => string)[]) => (s: string) => fns.reduceRight((acc, currFn) => currFn(acc), s)
+export const compose = <T>(...fns: CallbackFunction<T>[]) => (x: T) => fns.reduceRight((acc, currFn) => currFn(acc), x)
