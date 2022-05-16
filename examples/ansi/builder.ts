0bae425f8c31333efe0de645fa89874feb85e821
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-05-16 19:21:12 +0530

    Add builder example

---
+++ examples/ansi/builder.ts
@@ -0,0 +1,11 @@
+//  ANSI Template String Builder
+import { ansi } from '../../ansi/builder.ts'
+
+//  Helper Functions
+import { compose } from '../../helpers/composition.ts'
+import { red, blue } from '../../ansi/colors.ts'
+import { inverse, bold } from '../../ansi/styles.ts'
+
+console.log(
+    ansi`ANSI ${blue} Template ${inverse} String ${compose(red, bold)} Builder`
+)
