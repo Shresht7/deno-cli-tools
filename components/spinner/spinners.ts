999e7f482c6b24e456abc7171651787bc5908a22
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-05-18 00:27:21 +0530

    Add links to cli-spinners

--- components/spinner/spinners.ts
+++ components/spinner/spinners.ts
@@ -5,8 +5,12 @@ export interface SpinnerInterface {
 
 export type SpinnerType = keyof typeof spinners
 
-//  https://github.com/sindresorhus/cli-spinners
-
+/**
+ * Spinner Variants
+ * 
+ * - Source: {@link https://github.com/sindresorhus/cli-spinners}
+ * - License: MIT {@link https://github.com/sindresorhus/cli-spinners/blob/00de8fbeee16fa49502fa4f687449f70f2c8ca2c/license}
+ */
 export const spinners = {
     windows: {
         interval: 80,
