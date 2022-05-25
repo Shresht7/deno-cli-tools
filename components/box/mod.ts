59510a43240cfd9fc28b25ba74645408c199fae8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-05-25 22:32:13 +0530

    Add Box Title

--- components/box/mod.ts
+++ components/box/mod.ts
@@ -31,9 +31,32 @@ export function box({ contents, title, type = 'classic', alignment = 'center' }:
         .map(line => box.left + line + box.right)
 
     //  Add the top and bottom sections
-    arr.unshift(box.topLeft + box.top.repeat(maxWidth) + box.topRight)
+    title = align(' ' + title + ' ', { align: alignment, pad: box.top, width: maxWidth })
+    arr.unshift(box.topLeft + title + box.topRight)
     arr.push(box.bottomLeft + box.top.repeat(maxWidth) + box.bottomRight)
 
     //  Concatenate as string
     return arr.join('\n')
+
 }
+
+console.log(box({
+    title: 'Title Left',
+    contents: 'Contents Left',
+    type: 'double',
+    alignment: 'left'
+}))
+
+console.log(box({
+    title: 'Title Center',
+    contents: 'Contents Center',
+    type: 'double',
+    alignment: 'center'
+}))
+
+console.log(box({
+    title: 'Title Right',
+    contents: 'Contents Right',
+    type: 'double',
+    alignment: 'right'
+}))
