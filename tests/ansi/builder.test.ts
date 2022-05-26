1e5b194545b7dbd903be1c6f63c55d45dd84df1e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-05-26 22:34:40 +0530

    Add Builder Tests

---
+++ tests/ansi/builder.test.ts
@@ -0,0 +1,41 @@
+//  Library
+import { ansi } from '../../ansi/builder.ts'
+
+//  Testing
+import { assertEquals } from '../../library/asserts.ts'
+
+//  Helpers
+import { blue, black, white } from '../../ansi/colors.ts'
+import { bold } from '../../ansi/styles.ts'
+import { RESET } from '../../ansi/codes.ts'
+import { compose } from '../../helpers/composition.ts'
+
+
+Deno.test('ANSI Template String Builder', async (t) => {
+
+    await t.step('should return a string', () => assertEquals(typeof ansi`abcde`, 'string'))
+
+    await t.step('should allow string interpolation', () => {
+        const number = Math.floor(Math.random() * 10)
+        assertEquals(ansi`Random number: ${number}`, `Random number: ${number}`)
+    })
+
+    await t.step('should apply ansi codes using a ansi function', () => {
+        const actual = ansi`This is ${blue}BLUE`
+        const expected = 'This is \u001b[34mBLUE\u001b[39m'
+        assertEquals(actual, expected)
+    })
+
+    await t.step('should apply ansi codes using multiple ansi functions', () => {
+        const actual = ansi`Nothing is ever completely ${black}BLACK${RESET} or ${white}WHITE`
+        const expected = 'Nothing is ever completely \u001b[30mBLACK\u001b[39m\u001b[0m or \u001b[37mWHITE\u001b[39m'
+        assertEquals(actual, expected)
+    })
+
+    await t.step('should allow function composition', () => {
+        const actual = ansi`Mix ${compose(black, bold)}BLACK and BOLD`
+        const expected = 'Mix \u001b[30m\u001b[1mBLACK and BOLD\u001b[22m\u001b[39m'
+        assertEquals(actual, expected)
+    })
+
+})
