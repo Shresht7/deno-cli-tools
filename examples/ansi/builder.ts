c2929e8851dcca388d287f22ab872da1d2c832a2
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-05-27 11:23:35 +0530

    Update ANSI builder examples

--- examples/ansi/builder.ts
+++ examples/ansi/builder.ts
@@ -3,9 +3,34 @@ import { ansi } from '../../ansi/builder.ts'
 
 //  Helper Functions
 import { compose } from '../../helpers/composition.ts'
-import { red, blue } from '../../ansi/colors.ts'
-import { inverse, bold } from '../../ansi/styles.ts'
+import { RESET as reset } from '../../ansi/codes.ts'
+import { blue, yellow, magenta } from '../../ansi/colors.ts'
+import { inverse, bold, italic, faint, pad } from '../../ansi/styles.ts'
 
 console.log(
-    ansi`ANSI ${blue} Template ${inverse} String ${compose(red, bold)} Builder ${inverse("!").repeat(3)}`
+    ansi`
+    ${compose(bold, blue, inverse, pad(3))}# ANSI Template String Builder${reset}
+
+    The ${bold('ANSI builder API')} makes use of the power of ${compose(yellow, italic)}template string literals${reset}
+    to provide a simple and clean way to write ANSI strings.
+    
+    ${faint}Any ANSI helper function (or any function with ${compose(magenta, italic)}\`(s: string) => string\` ${faint}signature for that matter)
+    can be passed in the template strings that will be called upon the following string section (and only that section).
+    `
+)
+
+const header = compose(bold, blue, inverse, pad(3))
+const highlight = compose(yellow, italic)
+const code = compose(magenta, italic)
+
+console.log(
+    ansi`
+    ${header}# ANSI Template String Builder${reset}
+
+    The ${bold('ANSI builder API')} makes use of the power of ${highlight}template string literals${reset}
+    to provide a simple and clean way to write ANSI strings.
+    
+    ${faint}Any ANSI helper function (or any function with ${code}\`(s: string) => string\` ${faint}signature for that matter)
+    can be passed in the template strings that will be called upon the following string section (and only that section).
+    `.split('\n').map(line => line.trimStart()).join('\n')
 )
