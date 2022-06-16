fbc0b2ddd4caa63c31073aaa45246250d4abede2
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-16 22:07:40 +0530

    Add symbol examples

---
+++ symbols/symbols.example.ts
@@ -0,0 +1,6 @@
+import { symbol, status } from "./mod.ts"
+
+console.log(symbol.twoThirds, symbol.almostEqual, "0.67")
+console.log(symbol.arrowDown, symbol.arrowUp, symbol.arrowLeft, symbol.arrowRight)
+console.log(status.info, status.increase, "Increase")
+console.log(status.decrease, status.decrease, "Increase", status.warning)
