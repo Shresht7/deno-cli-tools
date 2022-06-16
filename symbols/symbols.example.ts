534cf2047e71a96f432452a9944bfce391ac35cd
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-16 22:08:57 +0530

    Fix symbol example

--- symbols/symbols.example.ts
+++ symbols/symbols.example.ts
@@ -1,6 +1,6 @@
 import { symbol, status } from "./mod.ts"
 
-console.log(symbol.twoThirds, symbol.almostEqual, "0.67")
-console.log(symbol.arrowDown, symbol.arrowUp, symbol.arrowLeft, symbol.arrowRight)
-console.log(status.info, status.increase, "Increase")
-console.log(status.decrease, status.decrease, "Increase", status.warning)
+console.log(symbol.twoThirds, symbol.almostEqual, "0.67")   //  ⅔ ≈ 0.67
+console.log(symbol.arrowDown, symbol.arrowUp, symbol.arrowLeft, symbol.arrowRight)  //  ↓ ↑ ← →
+console.log(status.info, status.increase, "Increase")   //  ⓘ ▲ Increase
+console.log(status.warning, status.decrease, "Decrease")    //  ⚠️ ▼ Decrease
