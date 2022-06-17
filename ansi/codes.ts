a7eeb7318b5cc0e710079e75a19103f25ce6fee6
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-17 11:38:43 +0530

    Support ANSI Code Regex

--- ansi/codes.ts
+++ ansi/codes.ts
@@ -28,22 +28,31 @@ export const BEL = '\u0007'
 interface Code {
     open: string
     close: string
+    regexp: {
+        open: RegExp
+        close: RegExp
+    }
 }
 
-/** Helper function to format the given ANSI code */
+/** Helper function to format the given ANSI codes */
 export const code = (open: number | number[], close: number): Code => {
     open = Array.isArray(open) ? open : [open]
     return {
         open: `${ESC}[${open.join(';')}m`,
         close: `${ESC}[${close}m`,
+        regexp: {
+            open: new RegExp(`${ESC}\\[${open.join(';')}m`),
+            close: new RegExp(`${ESC}\\[${close}m`)
+        }
     }
 }
 
 /**
- * Wrap ANSICode around string
+ * Wrap ANSI Codes around string
  * @param str text to wrap string around
- * @param tuple ansiCode tuple to wrap
+ * @param code ansi code to wrap
+ * @param enabled if false, return the string unaltered
  */
 export const wrap = (str: string, code: Code, enabled = true) => enabled
-    ? code.open + str + code.close
+    ? code.open + str.replace(code.regexp.close, code.open) + code.close
     : str
