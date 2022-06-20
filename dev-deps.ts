2c2cff050f89d43ff136c9e15770d461bbfe7636
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-20 12:00:37 +0530

    Update dependencies

--- dev-deps.ts
+++ dev-deps.ts
@@ -2,5 +2,5 @@
 //  DEPENDENCIES
 //  ============
 
-export * from 'https://deno.land/std@0.140.0/testing/asserts.ts'
+export * from 'https://deno.land/std@0.144.0/testing/asserts.ts'
 export * from "https://deno.land/std@0.144.0/path/mod.ts"
