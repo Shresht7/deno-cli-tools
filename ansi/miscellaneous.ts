c5129ba180c4410c80a02aa5f54c6c71852ef34c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-02 16:06:44 +0530

    Add docstrings

--- ansi/miscellaneous.ts
+++ ansi/miscellaneous.ts
@@ -5,16 +5,22 @@ import { OSC, BEL, CSI } from './codes.ts'
 //  MISCELLANEOUS
 //  =============
 
+/** Create a hyperlink */
 export const link = (text: string, url: string) => `${OSC}8;;${url}${BEL}${text}${OSC}8;;${BEL}` //  Returns a link
 
+/** Play a bell sound */
 export const bell = BEL
 
 export const screen = {
+    /** Save the screen */
     save: CSI + '?47h',
+    /** Restore the screen */
     restore: CSI + '?47l'
 }
 
 export const altBuffer = {
+    /** Enable the alternate buffer */
     enable: CSI + '?1049h',
+    /** Disable the alternate buffer */
     disable: CSI + '?1049l'
 }
