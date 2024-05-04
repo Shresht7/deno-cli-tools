455afd61866b03b593cce3a5a707d00f8f028125
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-04 15:06:27 +0530

    Add docstrings

--- ansi/builder.ts
+++ ansi/builder.ts
@@ -5,22 +5,33 @@
 /** Callback function that takes in a string and returns a modified version of that string */
 type StringModifier = (s: string) => string
 
-/** ANSI template string builder */
+
+/**
+ * Applies ANSI escape sequences to a template string.
+ * @param templateStr - The template string to apply ANSI escape sequences to.
+ * @param rest - Additional parameters that can be used as string modifiers or values to be inserted into the template string.
+ * @returns The resulting string with ANSI escape sequences applied.
+ * 
+ * @example
+ * ```ts
+ * ansi`This is ${blue}BLUE` // This is \u001b[34mBLUE\u001b[39m
+ * ```
+ */
 export const ansi = (templateStr: TemplateStringsArray, ...rest: (string | number | StringModifier)[]) => {
-    return templateStr.reduce((acc, curr, i) => {
+  return templateStr.reduce((acc, curr, i) => {
 
-        //  If the preceding parameter is a function, execute it on the current template string
-        if (typeof rest[i - 1] === 'function') {
-            const fn = rest[i - 1] as StringModifier
-            acc += fn(curr)
-        }
+    //  If the preceding parameter is a function, execute it on the current template string
+    if (typeof rest[i - 1] === 'function') {
+      const fn = rest[i - 1] as StringModifier
+      acc += fn(curr)
+    }
 
-        //  If the template parameter is a string then return normally
-        if (typeof rest[i - 1] === 'string' || typeof rest[i - 1] === 'number') {
-            acc += rest[i - 1].toString() || ''
-            acc += curr
-        }
+    //  If the template parameter is a string then return normally
+    if (typeof rest[i - 1] === 'string' || typeof rest[i - 1] === 'number') {
+      acc += rest[i - 1].toString() || ''
+      acc += curr
+    }
 
-        return acc
-    })
+    return acc
+  })
 }
